6c91b82d8d220be5a4b90bf6a4a284e497cbdc0e
Dhruv <ddrathod510@gmail.com>
2024-10-23 21:40:26 -0400

    reverse number

--- demo.js
+++ demo.js
@@ -1,1 +1,11 @@
-console.log("hello")
+// Write a JavaScript function that reverses a number.
+// Sample Data and output:
+// Example x = 32243;
+// Expected Output: 34223
+
+function revNumber(n) {
+  let x = n.toString();
+  return x.split("").reverse().join("");
+}
+
+console.log(revNumber(123));
